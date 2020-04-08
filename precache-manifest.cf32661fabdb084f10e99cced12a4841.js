5f3598358f1e7007ac08b420a7008585db7dfcfb
Archan <archan@macbook.local>
2020-04-07 22:15:36 -0700

    Initial commit

---
+++ precache-manifest.cf32661fabdb084f10e99cced12a4841.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e626147c36303add45d9ffea3085363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31c39e232f3e3b453af9",
+    "url": "/static/css/main.cf9acc9c.chunk.css"
+  },
+  {
+    "revision": "42b39d46cad16dac5718",
+    "url": "/static/js/2.a73f5358.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a73f5358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c39e232f3e3b453af9",
+    "url": "/static/js/main.9ba286f9.chunk.js"
+  },
+  {
+    "revision": "59e9b66fb6f23b54577e",
+    "url": "/static/js/runtime-main.ab8723fa.js"
+  },
+  {
+    "revision": "a65d966afe4a174192b480d6d22d1a92",
+    "url": "/static/media/archan.a65d966a.png"
+  },
+  {
+    "revision": "5693d20a5dab47af6b6137c257e1a3d3",
+    "url": "/static/media/background2.5693d20a.jpeg"
+  }
+]);
