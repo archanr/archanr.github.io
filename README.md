81f2be291d8b65502a3a33f38a8d21b2229a0fd2
Archan <archan@macbook.local>
2020-04-29 22:16:30 -0700

    updating

---
+++ README.md
@@ -0,0 +1,2 @@
+# archanr.github.io
+Personal Website created using HTML/CSS/JavaScript with React Library
---
+++ precache-manifest.b438c725335b28e53ce07f8be3b667cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc5eb584357594af383d353da5cdf6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd8a39e51ccc6a4fafe2",
+    "url": "/static/css/main.49b8d455.chunk.css"
+  },
+  {
+    "revision": "95dfafc95b22d9308fd7",
+    "url": "/static/js/2.c3c5707d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3c5707d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8a39e51ccc6a4fafe2",
+    "url": "/static/js/main.268b15b2.chunk.js"
+  },
+  {
+    "revision": "59e9b66fb6f23b54577e",
+    "url": "/static/js/runtime-main.ab8723fa.js"
+  },
+  {
+    "revision": "a65d966afe4a174192b480d6d22d1a92",
+    "url": "/static/media/archan.a65d966a.png"
+  }
+]);
