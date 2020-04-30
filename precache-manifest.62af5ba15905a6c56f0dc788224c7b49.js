a625128b4fe7f011a978d77d26d2771e2c69f6a2
Archan <archan@macbook.local>
2020-04-29 23:31:25 -0700

    website 3.0

---
+++ precache-manifest.62af5ba15905a6c56f0dc788224c7b49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1d437e5edbfe0bee344c6c219ba155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17783f8c668fbb95ac76",
+    "url": "/static/css/main.25e92cf2.chunk.css"
+  },
+  {
+    "revision": "95dfafc95b22d9308fd7",
+    "url": "/static/js/2.c3c5707d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3c5707d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17783f8c668fbb95ac76",
+    "url": "/static/js/main.75883293.chunk.js"
+  },
+  {
+    "revision": "59e9b66fb6f23b54577e",
+    "url": "/static/js/runtime-main.ab8723fa.js"
+  },
+  {
+    "revision": "a65d966afe4a174192b480d6d22d1a92",
+    "url": "/static/media/archan.a65d966a.png"
+  }
+]);
