3f4794725a80611ece5ef8f4a1774f110e39d16c
Archan <archan@Archans-MacBook-Air.local>
2021-01-21 11:15:06 -0800

    updating text

---
+++ README.md
@@ -0,0 +1,9 @@
+# Buil app for personal landing page
+
+## Created by Archan Rupela | Last Updated June 2020
+## Created using HTML, CSS, and JavaScript with React Library
+
+#### View:
+1. Visit "https://archanrupela.com"
+
+
---
+++ precache-manifest.daa77761afbe2b92c74362ab759185fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba67d827d3187d9245a6e53dc99d4b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d46db9a658bd56a656c7",
+    "url": "/static/css/main.987823e8.chunk.css"
+  },
+  {
+    "revision": "b973c604dd7bde3f1a75",
+    "url": "/static/js/2.77b99424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77b99424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46db9a658bd56a656c7",
+    "url": "/static/js/main.5ab2eeab.chunk.js"
+  },
+  {
+    "revision": "6f66a38a23320cf835ab",
+    "url": "/static/js/runtime-main.1b2b1293.js"
+  },
+  {
+    "revision": "d32f9ba82b585d429b4f73ab82502147",
+    "url": "/static/media/archan.d32f9ba8.png"
+  }
+]);
